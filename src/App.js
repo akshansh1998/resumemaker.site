5db1b02ac69002ab1091d1083d76e480ac27df08
Ankush Anand <ankushanand718@gmail.com>
2023-01-28 15:50:57 +0530

    First Commit - Installed React
    Co-Authored-By: Praveen Singh Chauhan <122686109+chauhanpraveen@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ResumeMaker.site is Coming Soon
+      </header>
+    </div>
+  );
+}
+
+export default App;
